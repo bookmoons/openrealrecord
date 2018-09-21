dbf77e47fc5d4c0ead444138d5f61508e07fd8e2
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-21 18:21:50 -0400

    module/relay: Use send* receive* for message delivery routines
    Using "send" for both directions was too confusing.

--- test/unit/module/base/relay.js
+++ test/unit/module/base/relay.js
@@ -4,7 +4,7 @@ const dummyFunction = require('dummy/function')
 const ModuleRelay = require('module/base/relay')
 
 tape('construct', t => {
-  const prot = { stopSender: dummyFunction }
+  const prot = { sendStop: dummyFunction }
   const exposer = sinon.spy()
   new ModuleRelay(prot, exposer) /* eslint-disable-line no-new */
   t.true(exposer.calledOnce, 'exposer called')
@@ -12,39 +12,39 @@ tape('construct', t => {
 })
 
 tape('expose', t => {
-  const prot = { stopSender: dummyFunction }
+  const prot = { sendStop: dummyFunction }
   /* eslint-disable-next-line no-new */
   new ModuleRelay(
     prot,
-    function receiveRelayProtected ({ doneSender }) {
-      t.is(typeof doneSender, 'function', 'done sender exposed')
+    function receiveRelayProtected ({ receiveDone }) {
+      t.is(typeof receiveDone, 'function', 'done sender exposed')
     }
   )
   t.end()
 })
 
 tape('stop', t => {
-  const prot = { stopSender: sinon.spy() }
+  const prot = { sendStop: sinon.spy() }
   const exposer = sinon.fake()
   const relay = new ModuleRelay(prot, exposer)
   relay.stop().then(() => {
-    t.true(prot.stopSender.calledOnce, 'stop sender called')
+    t.true(prot.sendStop.calledOnce, 'stop sender called')
     t.end()
   })
 })
 
 tape('done', t => {
-  const prot = { stopSender: dummyFunction }
-  let doneSender
+  const prot = { sendStop: dummyFunction }
+  let receiveDone
   const relay = new ModuleRelay(
     prot,
     function receiveRelayProtected (prot) {
-      doneSender = prot.doneSender
+      receiveDone = prot.receiveDone
     }
   )
   relay.done.then(() => {
     t.pass('done delivered')
     t.end()
   })
-  doneSender()
+  receiveDone()
 })
