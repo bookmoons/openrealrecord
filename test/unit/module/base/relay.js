6531e01660eaa8761479b24ab11bd11950ad68d2
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-21 18:47:16 -0400

    module: Name message delivery deliver* in interface
    It's awkward when they're named from the perspective of the
    object that provides them.

--- test/unit/module/base/relay.js
+++ test/unit/module/base/relay.js
@@ -4,7 +4,7 @@ const dummyFunction = require('dummy/function')
 const ModuleRelay = require('module/base/relay')
 
 tape('construct', t => {
-  const prot = { sendStop: dummyFunction }
+  const prot = { deliverStop: dummyFunction }
   const exposer = sinon.spy()
   new ModuleRelay(prot, exposer) /* eslint-disable-line no-new */
   t.true(exposer.calledOnce, 'exposer called')
@@ -12,38 +12,38 @@ tape('construct', t => {
 })
 
 tape('expose', t => {
-  const prot = { sendStop: dummyFunction }
+  const prot = { deliverStop: dummyFunction }
   /* eslint-disable-next-line no-new */
   new ModuleRelay(
     prot,
-    function receiveRelayProtected ({ receiveDone }) {
-      t.is(typeof receiveDone, 'function', 'done sender exposed')
+    function receiveRelayProtected ({ deliverDone }) {
+      t.is(typeof deliverDone, 'function', 'done sender exposed')
     }
   )
   t.end()
 })
 
 tape('stop', t => {
-  const prot = { sendStop: sinon.spy() }
+  const prot = { deliverStop: sinon.spy() }
   const exposer = sinon.fake()
   const relay = new ModuleRelay(prot, exposer)
   relay.stop()
-  t.true(prot.sendStop.calledOnce, 'stop sender called')
+  t.true(prot.deliverStop.calledOnce, 'stop sender called')
   t.end()
 })
 
 tape('done', t => {
-  const prot = { sendStop: dummyFunction }
-  let receiveDone
+  const prot = { deliverStop: dummyFunction }
+  let deliverDone
   const relay = new ModuleRelay(
     prot,
     function receiveRelayProtected (prot) {
-      receiveDone = prot.receiveDone
+      deliverDone = prot.deliverDone
     }
   )
   relay.done.then(() => {
     t.pass('done delivered')
     t.end()
   })
-  receiveDone()
+  deliverDone()
 })
