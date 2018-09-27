1fd30eb7153f7037d7a634fd5ded5026dd95a29b
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-26 21:21:17 -0400

    connector/btc: Add BcoinBitcoinConnector

---
+++ test/mul/connector/bitcoin/bcoin.js
@@ -0,0 +1,75 @@
+const test = require('ava')
+const bcoin = require('bcoin')
+const Delay = require('helper/delay')
+const BcoinBitcoinBlockchainFixture =
+  require('fixture/blockchain/bitcoin/bcoin')
+const StaticBitcoinFeeProvider = require('provider/bitcoin/fee/static')
+const BcoinBitcoinConnector = require('connector/bitcoin/bcoin')
+
+const regtest = bcoin.Network.get('regtest')
+
+test.beforeEach(async t => {
+  const blockchain = new BcoinBitcoinBlockchainFixture()
+  await blockchain.setup()
+  t.context.blockchain = blockchain
+})
+
+test.afterEach(async t => {
+  if (t.context.blockchain) await t.context.blockchain.teardown()
+})
+
+test('publish', async t => {
+  const blockchain = t.context.blockchain
+  const feeProvider = new StaticBitcoinFeeProvider(1000000) // 0.01 BTC
+  const connector = new BcoinBitcoinConnector({
+    network: regtest,
+    feeProvider,
+    wallet: {
+      apiKey: blockchain.walletNodeApiKey,
+      id: blockchain.walletId,
+      passphrase: blockchain.walletPassphrase,
+      account: blockchain.walletAccount
+    }
+  })
+  const data = Buffer.from([ 0x01, 0x02, 0x03 ])
+  const txid = await connector.publishData(data)
+  t.is(typeof txid, 'string')
+  t.is(txid.length, 64)
+  t.regex(txid, /[0-9a-fA-F]{2}/g) // Hexadecimal string
+  const confirmedPromise = new Promise((resolve, reject) => {
+    function handleTimeout () {
+      blockchain.chainNode.removeListener('block', checkBlockForTx)
+      reject(new Error('timed out'))
+    }
+    function checkBlockForTx (block) {
+      if (block.txs.length < 2) return
+      for (const tx of block.txs) {
+        if (tx.txid() === txid) {
+          blockchain.chainNode.removeListener('block', checkBlockForTx)
+          resolve(tx)
+          return
+        }
+      }
+    }
+    Delay.seconds(1).then(handleTimeout)
+    blockchain.chainNode.on('block', checkBlockForTx)
+  })
+  await blockchain.chainNodeClient.execute('generate', [ 1 ])
+  await t.notThrowsAsync(confirmedPromise, 'tx published')
+  const tx = await confirmedPromise
+  const output = (function extractOutput () {
+    for (const output of tx.outputs) {
+      if (output.getType() === 'nulldata') return output
+      return null
+    }
+  })()
+  t.truthy(output, 'tx has data')
+  const script = output.script
+  const code = script.code
+  t.is(code[0].value, 106, 'op_return output')
+  t.deepEqual(
+    code[1].data,
+    Buffer.from([ 0x01, 0x02, 0x03 ]),
+    'correct data published'
+  )
+})
